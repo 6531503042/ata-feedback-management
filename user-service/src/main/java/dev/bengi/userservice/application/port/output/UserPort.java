ecb91433b76a8bc245104146780fdac06132d2ca
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-24 00:01:18 +0700

    User-Service: Updated appliaction output.

--- user-service/src/main/java/dev/bengi/userservice/application/port/output/UserPort.java
+++ user-service/src/main/java/dev/bengi/userservice/application/port/output/UserPort.java
@@ -2,11 +2,17 @@ package dev.bengi.userservice.application.port.output;
 
 import dev.bengi.userservice.domain.model.User;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.List;
 
 public interface UserPort {
     User save(User user);
 
+    Optional<User> findById(UUID id);
     Optional<User> findByEmail(String email);
 
+    List<User> findAll();
+
+    void deleteById(UUID id);
     boolean existsByEmail(String email);
 }
