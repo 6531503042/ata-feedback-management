8c566366e938364a09af0c0a7eb9ca355b5687f6
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-27 18:52:26 +0700

    Feedback-Service: Updated main runner files.

--- feedback-service/src/main/java/dev/bengi/feedbackservice/FeedbackServiceApplication.java
+++ feedback-service/src/main/java/dev/bengi/feedbackservice/FeedbackServiceApplication.java
@@ -2,8 +2,15 @@ package dev.bengi.feedbackservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {
+    "dev.bengi.feedbackservice.domain.model",
+    "dev.bengi.feedbackservice.infrastructure.persistence.entity"
+})
+@EnableJpaRepositories(basePackages = "dev.bengi.feedbackservice.infrastructure.persistence.repository")
 public class FeedbackServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(FeedbackServiceApplication.class, args);
