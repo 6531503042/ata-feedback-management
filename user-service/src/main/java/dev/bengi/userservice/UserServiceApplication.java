f2125c8895ce516008c88a522ef94ec4d14172d0
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-27 18:45:04 +0700

    User-Service: Updated runner as eureka-discovery

--- user-service/src/main/java/dev/bengi/userservice/UserServiceApplication.java
+++ user-service/src/main/java/dev/bengi/userservice/UserServiceApplication.java
@@ -2,17 +2,10 @@ package dev.bengi.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EntityScan(basePackages = {
-        "dev.bengi.userservice.domain.model",
-        "dev.bengi.userservice.infrastructure.persistence.entity"
-})
-@EnableJpaRepositories(basePackages = {
-        "dev.bengi.userservice.infrastructure.persistence.repository"
-})
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {
