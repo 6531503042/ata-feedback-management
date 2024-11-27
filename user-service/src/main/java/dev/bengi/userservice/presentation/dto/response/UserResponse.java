0215dd13a582220e5811c35410b3bfebe7144c21
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-27 18:44:33 +0700

    User-Service: Updated DTO Layer.

--- user-service/src/main/java/dev/bengi/userservice/presentation/dto/response/UserResponse.java
+++ user-service/src/main/java/dev/bengi/userservice/presentation/dto/response/UserResponse.java
@@ -1,6 +1,5 @@
 package dev.bengi.userservice.presentation.dto.response;
 
-import dev.bengi.userservice.domain.model.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,12 +9,13 @@ import java.util.UUID;
 
 @Data
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserResponse {
     private UUID id;
+    private String email;
     private String firstName;
     private String lastName;
-    private String email;
-    private Role role;
+    private String role;
+    private boolean active;
 }
