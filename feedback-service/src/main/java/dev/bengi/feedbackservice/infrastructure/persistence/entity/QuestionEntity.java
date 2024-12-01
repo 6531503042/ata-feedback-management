e25584c646f50c3b42d9e8b575824593b4fb06ed
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-12-01 18:25:08 +0700

    Feedback-Service: Adjust Entity

--- feedback-service/src/main/java/dev/bengi/feedbackservice/infrastructure/persistence/entity/QuestionEntity.java
+++ feedback-service/src/main/java/dev/bengi/feedbackservice/infrastructure/persistence/entity/QuestionEntity.java
@@ -2,13 +2,13 @@ package dev.bengi.feedbackservice.infrastructure.persistence.entity;
 
 import dev.bengi.feedbackservice.domain.model.enums.QuestionCategory;
 import dev.bengi.feedbackservice.domain.model.enums.QuestionType;
-import dev.bengi.feedbackservice.domain.model.enums.QuestionSentiment;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -23,28 +23,28 @@ public class QuestionEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(nullable = false)
     private String text;
     private String content;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private QuestionCategory category;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private QuestionType type;
 
-    @Enumerated(EnumType.STRING)
-    private QuestionSentiment sentiment;
+    private String answerType;
+    private boolean sentimentAnalysis;
+    private boolean required;
+    private boolean active;
 
     @ElementCollection
     @CollectionTable(name = "question_options", 
         joinColumns = @JoinColumn(name = "question_id"))
-    @Column(name = "option")
-    private List<String> options;
-
-    private boolean required;
-    private boolean active;
-    private String wordBasedPrompt;
-    private String answerType;
+    @Builder.Default
+    private List<String> options = new ArrayList<>();
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "question_set_id")
